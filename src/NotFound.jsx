7e643caf1bfca17979656e04fa3a941b93f93e06
Monali Gupta <guptamonali61@gmail.com>
2023-08-04 23:46:11 +0530

    not-found image fix

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import NotFoundImage from "./not-found.jpg"
 function NotFound() {
   return (
     <div className="flex items-center justify-center h-full">
       <img
         className=""
-        src="https://t4.ftcdn.net/jpg/03/88/63/83/360_F_388638369_wSBADhKfhiTx6Q5Pz1xfdpy6zotku1Sg.jpg"
+        src={NotFoundImage}
       />
     </div>
   );
