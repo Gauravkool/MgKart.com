a35ce57bdaaeb4a1167116e9f62bcec8f07f89bd
Monali Gupta <guptamonali61@gmail.com>
2023-08-11 23:30:22 +0530

    update cart products ids

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProductData } from "./API";
 function CartPage({ cart }) {
+  const [products, setProducts] = useState([]);
   const productIds = Object.keys(cart);
-  console.log("product ids",cart, productIds);
-  return <div>Cart page</div>;
+  console.log("products outside", products.length);
+  useEffect(function () {
+    const myProductsPromises = productIds.map(function (id) {
+      return getProductData(id);
+    });
+    Promise.all(myProductsPromises).then(function (products) {
+      setProducts(products);
+    });
+  }, []);
+
+  return (
+    <div>
+      {products.map(function (p) {
+        return <div key={p.id}>{p.title}</div>;
+      })}
+    </div>
+  );
 }
 export default CartPage;
