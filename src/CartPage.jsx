f7e4f79debc92e2879eb45efea9a44097283bde5
Monali Gupta <guptamonali61@gmail.com>
2023-08-13 23:26:37 +0530

    change approach inncartpage to set handleChange and handleRemove function

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -7,8 +7,8 @@ function CartPage({ cart, updateCart }) {
   const [loading, setLoading] = useState(true);
   const [localCart, setLocalCart] = useState(cart);
   const productIds = Object.keys(cart);
-  
-  console.log(localCart)
+
+  console.log(localCart);
   useEffect(
     function () {
       setLocalCart(cart);
@@ -17,6 +17,7 @@ function CartPage({ cart, updateCart }) {
   );
   useEffect(
     function () {
+      setLoading(true);
       const myProductsPromises = productIds.map(function (id) {
         return getProductData(id);
       });
@@ -28,20 +29,15 @@ function CartPage({ cart, updateCart }) {
     [cart]
   );
 
-  function handleRemove(e) {
-    const productId = e.currentTarget.getAttribute("productid");
-    console.log("remove products by id", productId);
+  function handleRemove(productId) {
     const newCart = { ...cart };
     console.log("before cart", newCart);
     delete newCart[productId];
     updateCart(newCart);
     console.log("after cart", newCart);
-    setLoading(true);
   }
 
-  function handleChange(e) {
-    const newValue = +e.target.value;
-    const productId = e.target.getAttribute("productId");
+  function handleChange(newValue, productId) {
     console.log("newValue", " productid", newValue, productId);
     const newLocalCart = { ...localCart, [productId]: newValue };
     setLocalCart(newLocalCart);
@@ -61,22 +57,25 @@ function CartPage({ cart, updateCart }) {
           <div key={p.id}>
             {p.title}
             <input
-              productid={p.id}
               value={localCart[p.id]}
               type="number"
-              onChange={handleChange}
+              onChange={function (e) {
+                handleChange(+e.target.value, p.id);
+              }}
               className="w-12 p-1 border border-gray-200 rounded-md mx-2"
             />
-            <button productid={p.id} onClick={handleRemove}>
+            <button
+              productid={p.id}
+              onClick={function () {
+                handleRemove(p.id);
+              }}
+            >
               <AiOutlineCloseCircle />
             </button>
           </div>
         );
       })}
-      <button
-        className="bg-indigo-700"
-        onClick={handleUpdateCart}
-      >
+      <button className="bg-indigo-700" onClick={handleUpdateCart}>
         Update cart
       </button>
     </div>
